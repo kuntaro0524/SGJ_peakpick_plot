1b5cd963b925833359c55179a404f8958571eafc
Kunio Hirata <hsuser@kundev.spring8.or.jp>
2022-01-26 22:08:31 +0900

    実際の回転方向とズレを計算し、そのデータに対して同じ式でフィッティングをかけれるようにした。 実験ではこのスクリプトを使うことはなかった。 次に検討をするとしたらカメラの設置が光軸から何度ずれているか、というパラメタも入れたほうが良さそう。

---
+++ RotationCenter/sim_actual.py
@@ -0,0 +1,31 @@
+import os,sys
+import numpy as np
+from matplotlib import pyplot as plt
+
+# 回転中心を下流の顕微鏡カメラで覗いたときの左右の動きとX,Z座標の関係をシミュレートする
+# 初期 X,Zの座標 [mm]
+x = 10.0
+z = 1.0
+
+# これから計算できる原点からの距離
+r = np.sqrt(x**2+z**2)
+#print(r)
+
+# このX,Zがピント方向となす角度
+theta0 = np.arctan(z/x) # [rad.]
+
+x=[]
+y=[]
+ofile = open("sim.dat","w")
+#こいつをハード的にdthetaずつ回したときの挙動
+for theta_deg in range(0,360,5):
+    dtheta = np.radians(theta_deg)
+    d_dist = r*np.sin(theta0) - r*np.sin(theta0-dtheta) + np.random.rand(1)[0]
+    x.append(theta_deg)
+    y.append(d_dist)
+    ofile.write("%s %s\n" % (theta_deg, d_dist))
+
+xa=np.array(x)
+ya=np.array(y)
+plt.plot(xa,ya,'o-')
+plt.show()
